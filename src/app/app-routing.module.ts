076e47cf0f4351b82c8d648eee5db733db734a76
RenzoBTL30 <90786322+RenzoBTL30@users.noreply.github.com>
2022-11-02 23:27:56 -0500

    interfaz-login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { LoginModule } from './login/login.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    LoginModule
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }

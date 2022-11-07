9fe286f208eaf537a017e7e522fd02a83da0e880
digi85 <73599904+Digi85@users.noreply.github.com>
2022-11-07 13:51:35 -0500

    update

--- src/app/estudiante/menu-principal/menu-principal.module.ts
+++ src/app/estudiante/menu-principal/menu-principal.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenuPrincipalComponent } from './menu-principal.component';
+import { ElementosModule } from 'src/app/elementos/elementos.module';
 
 
 
 @NgModule({
   declarations: [
-    MenuPrincipalComponent
+    MenuPrincipalComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ElementosModule
   ],
   exports:[
     MenuPrincipalComponent

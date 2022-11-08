7cb3ac0d8ae07c7548093978de1212490dd96158
RenzoBTL30 <90786322+RenzoBTL30@users.noreply.github.com>
2022-11-08 17:45:39 -0500

    cambios xD

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
+import { RouterLink } from '@angular/router';
 
 
 
@@ -9,7 +10,11 @@ import { LoginComponent } from './login.component';
     LoginComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterLink
+  ],
+  exports:[
+    LoginComponent
   ]
 })
 export class LoginModule { }

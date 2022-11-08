d0695c5cca4a935a5895c2d530830fdf45a4d27f
RenzoBTL30 <90786322+RenzoBTL30@users.noreply.github.com>
2022-11-07 20:55:27 -0500

    registrar solicitud diseño v1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { LoginModule } from './login/login.module';
+import { RegistrarSolicitudComponent } from './registrar-solicitud/registrar-solicitud.component';
 
 const routes: Routes = [
-  { path: '', component: LoginComponent }
+  { path: '', component: LoginComponent },
+
+
+  { path: 'registrar-solicitud', component: RegistrarSolicitudComponent }
 ];
 
 @NgModule({

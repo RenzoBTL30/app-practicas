6521d8c8eee32c584b47259e13865acb38c49993
RenzoBTL30 <90786322+RenzoBTL30@users.noreply.github.com>
2022-11-07 22:37:14 -0500

    diseño-revisar-detalle solicitud

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MenuPrincipalComponent } from './estudiante/menu-principal/menu-principal.component';
 import { MenuPrincipalModule } from './estudiante/menu-principal/menu-principal.module';
+import { DetalleSolicitudComponent } from './estudiante/revisar-solicitudes/detalle-solicitud/detalle-solicitud.component';
+import { RevisarSolicitudesComponent } from './estudiante/revisar-solicitudes/revisar-solicitudes.component';
 import { LoginComponent } from './login/login.component';
 import { LoginModule } from './login/login.module';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
-  { path: 'menu-principal', component: MenuPrincipalComponent }
+  { path: 'menu-principal', component: MenuPrincipalComponent },
+  { path: 'revisar-solicitud', component: RevisarSolicitudesComponent },
+  { path: 'detalle-solicitud', component: DetalleSolicitudComponent }
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RevisarSolicitudesComponent } from './estudiante/revisar-solicitudes/revisar-solicitudes.component';
+import { DetalleSolicitudComponent } from './estudiante/revisar-solicitudes/detalle-solicitud/detalle-solicitud.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RevisarSolicitudesComponent
+    RevisarSolicitudesComponent,
+    DetalleSolicitudComponent
   ],
   imports: [
     BrowserModule,

9b2cea554980bbb2821fdb29bbde963191133533
RenzoBTL30 <90786322+RenzoBTL30@users.noreply.github.com>
2022-11-07 20:48:23 -0500

    revisar solicitudes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RevisarSolicitudesComponent } from './estudiante/revisar-solicitudes/revisar-solicitudes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RevisarSolicitudesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/estudiante/revisar-solicitudes/revisar-solicitudes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-revisar-solicitudes',
+  templateUrl: './revisar-solicitudes.component.html',
+  styleUrls: ['./revisar-solicitudes.component.css']
+})
+export class RevisarSolicitudesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

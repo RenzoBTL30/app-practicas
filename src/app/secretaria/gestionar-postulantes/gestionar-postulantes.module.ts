7e1afba5f2b41ce37f6976d20a45f9634965976a
Josiascrack <73666351+Josiascrack@users.noreply.github.com>
2022-11-17 18:01:47 -0500

    listo

--- src/app/secretaria/gestionar-postulantes/gestionar-postulantes.module.ts
+++ src/app/secretaria/gestionar-postulantes/gestionar-postulantes.module.ts
@@ -1,13 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatSelect, MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule, MAT_FORM_FIELD_DEFAULT_OPTIONS} from '@angular/material/form-field';
 import { GestionarPostulantesComponent } from './gestionar-postulantes.component';
+import { MatOptionModule } from '@angular/material/core';
 
 @NgModule({
   declarations: [
     GestionarPostulantesComponent
   ],
+  providers: [
+    {provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: {appearance: 'fill'}}
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatSelectModule,
+    MatSelectModule,
+    MatOptionModule,
+    MatFormFieldModule
   ],
   exports: [
     GestionarPostulantesComponent
